e6c27c9101e77e9faf9b2d5a4504510bd81e56ae
Luis <luis@lmseo.co>
2023-02-26 20:21:00 -0800

    added multilevel bootstrap navbar css based

--- wp-content/themes/helios/lib/partials/featured-banner.php
+++ wp-content/themes/helios/lib/partials/featured-banner.php
@@ -23,4 +23,118 @@ $out.='<div class="small-12">
     </div>
   </div>
 </div>';
+/*<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <nav class="navbar navbar-expand-lg bg-light">
+  <ul class="title-area">
+    <li class="name">
+        <h1 class="site-title" itemprop="headline">
+            <a href="//localhost:3000/" class="logo">LMSEO</a>
+        </h1>
+    </li>
+    <li class="toggle-topbar menu-icon">
+        <a href="#"><span>Menu</span></a>
+    </li>
+  </ul>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul id="primary-links" class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li id="nav-menu-item-544" class="navbar-main nav-item dropdown nav-item-even nav-item-depth-0 menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children has-dropdown not-click">
+            <a href="//localhost:3000/services/" class="menu-link nav-link dropdown-toggle">
+                Services
+            </a>
+            <ul class="dropdown-menu dropdown-odd  dropdown-depth-1">
+	            <li id="nav-menu-item-546" class=" nav-item dropdown nav-item-odd nav-item-depth-1 menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children hvr-buzz-out has-dropdown not-click">
+	                <a href="//localhost:3000/services/ecommerce/" class="menu-link nav-link dropdown-toggle">
+	                    Ecommerce
+	                </a>
+	                <ul class="dropdown-menu dropdown-even dropdown-child dropdown-depth-2">
+		                <li id="nav-menu-item-547" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out">
+		                    <a href="//localhost:3000/services/ecommerce/set-up/" class="menu-link dropdown-item">
+		                        Ecommerce Store Set Up
+		                    </a>
+		                </li>
+		                <li id="nav-menu-item-548" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out">
+		                    <a href="//localhost:3000/services/ecommerce/structured-data-markup/" class="menu-link dropdown-item">
+		                        Structured Data Markup Testing, Set up and Optimization
+		                    </a>
+		                </li>
+	                </ul>
+                </li>
+                <li id="nav-menu-item-549" class=" nav-item dropdown nav-item-odd nav-item-depth-1 menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children hvr-buzz-out has-dropdown not-click">
+                    <a href="//localhost:3000/services/google-services/" class="menu-link nav-link dropdown-toggle">
+                        Google Services
+                    </a>
+	                <ul class="dropdown-menu dropdown-even dropdown-child dropdown-depth-2">
+		                <li id="nav-menu-item-550" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out">
+		                    <a href="//localhost:3000/services/google-services/adsense/" class="menu-link dropdown-item">
+		                        Adsense
+		                    </a>
+		                </li>
+		<li id="nav-menu-item-551" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/google-services/adwords/" class="menu-link dropdown-item">Adwords</a></li>
+		<li id="nav-menu-item-552" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/google-services/google-analytics/" class="menu-link dropdown-item">Google Analytics</a></li>
+		<li id="nav-menu-item-553" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/google-services/google-apis/" class="menu-link dropdown-item">Google APIs and Tools</a></li>
+		<li id="nav-menu-item-554" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/google-services/google-merchant-center/" class="menu-link dropdown-item">Google Merchant Center</a></li>
+		<li id="nav-menu-item-555" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/google-services/google-my-business/" class="menu-link dropdown-item">Google My Business</a></li>
+		<li id="nav-menu-item-556" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/google-services/google-plus/" class="menu-link dropdown-item">Google Plus</a></li>
+		<li id="nav-menu-item-557" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/google-services/google-webmaster-tools/" class="menu-link dropdown-item">Google Webmaster Tools</a></li>
+	</ul>
+</li>
+	<li id="nav-menu-item-558" class=" nav-item dropdown nav-item-odd nav-item-depth-1 menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children hvr-buzz-out has-dropdown not-click"><a href="//localhost:3000/services/hosting/" class="menu-link nav-link dropdown-toggle">Hosting</a>
+	<ul class="dropdown-menu dropdown-even dropdown-child dropdown-depth-2">
+		<li id="nav-menu-item-559" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/hosting/amazon/" class="menu-link dropdown-item">Amazon Web services Set Up</a></li>
+		<li id="nav-menu-item-560" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/hosting/cdn/" class="menu-link dropdown-item">Content Delivery Network(CDN)</a></li>
+		<li id="nav-menu-item-561" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/hosting/developer-environment/" class="menu-link dropdown-item">Developer Environment</a></li>
+		<li id="nav-menu-item-562" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/hosting/managed-hosting/" class="menu-link dropdown-item">Managed Hosting and Support</a></li>
+		<li id="nav-menu-item-563" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/hosting/security-certificates/" class="menu-link dropdown-item">SSL Certificates</a></li>
+		<li id="nav-menu-item-564" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/hosting/set-up/" class="menu-link dropdown-item">Virtual, Shared or Dedicated Hosting Set up</a></li>
+	</ul>
+</li>
+	<li id="nav-menu-item-565" class=" nav-item dropdown nav-item-odd nav-item-depth-1 menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children hvr-buzz-out has-dropdown not-click"><a href="//localhost:3000/services/marketing/" class="menu-link nav-link dropdown-toggle">Marketing</a>
+	<ul class="dropdown-menu dropdown-even dropdown-child dropdown-depth-2">
+		<li id="nav-menu-item-566" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/marketing/email/" class="menu-link dropdown-item">Email Marketing</a></li>
+		<li id="nav-menu-item-567" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/marketing/information-architecture/" class="menu-link dropdown-item">Information Architecture</a></li>
+		<li id="nav-menu-item-568" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/marketing/sem/" class="menu-link dropdown-item">Pay Per Click and Media Buying</a></li>
+		<li id="nav-menu-item-569" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/marketing/press-releases/" class="menu-link dropdown-item">Press Releases</a></li>
+		<li id="nav-menu-item-570" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/marketing/schema/" class="menu-link dropdown-item">Schema Markup Set up and Optimization</a></li>
+		<li id="nav-menu-item-571" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/marketing/search-engine-optimization/" class="menu-link dropdown-item">Search Engine Optimization</a></li>
+		<li id="nav-menu-item-572" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/marketing/social-media/" class="menu-link dropdown-item">Social Media</a></li>
+		<li id="nav-menu-item-583" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/web-development/front-end-development/email-white-paper/" class="menu-link dropdown-item">Email and White Paper</a></li>
+		<li id="nav-menu-item-545" class=" nav-item dropdown nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children hvr-buzz-out has-dropdown not-click"><a href="//localhost:3000/services/design/" class="menu-link nav-link dropdown-toggle">Design</a>
+		<ul class="dropdown-menu dropdown-odd dropdown-child dropdown-depth-3">
+			<li id="nav-menu-item-584" class="  nav-item-odd nav-item-depth-3 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/web-development/front-end-development/logo-and-business-cards/" class="menu-link dropdown-item">Logo and Business Cards</a></li>
+		</ul>
+</li>
+	</ul>
+</li>
+	<li id="nav-menu-item-573" class=" nav-item dropdown nav-item-odd nav-item-depth-1 menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children hvr-buzz-out has-dropdown not-click"><a href="//localhost:3000/services/web-development/" class="menu-link nav-link dropdown-toggle">Web Development</a>
+	<ul class="dropdown-menu dropdown-even dropdown-child dropdown-depth-2">
+		<li id="nav-menu-item-574" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/web-development/back-end-development/" class="menu-link dropdown-item">Back-end Development</a></li>
+		<li id="nav-menu-item-575" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/web-development/back-end-development/content-management-system/" class="menu-link dropdown-item">Content Management System (WordPress, Drupal)</a></li>
+		<li id="nav-menu-item-576" class=" nav-item dropdown nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children hvr-buzz-out has-dropdown not-click"><a href="//localhost:3000/services/web-development/back-end-development/database-management/" class="menu-link nav-link dropdown-toggle">Database Management</a>
+		<ul class="dropdown-menu dropdown-odd dropdown-child dropdown-depth-3">
+			<li id="nav-menu-item-577" class="  nav-item-odd nav-item-depth-3 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/web-development/back-end-development/database-management/database-design/" class="menu-link dropdown-item">Database Design</a></li>
+			<li id="nav-menu-item-578" class="  nav-item-odd nav-item-depth-3 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/web-development/back-end-development/database-management/database-testing-optimization/" class="menu-link dropdown-item">Database Testing and Optimization</a></li>
+			<li id="nav-menu-item-579" class="  nav-item-odd nav-item-depth-3 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/web-development/back-end-development/database-management/database-set-up/" class="menu-link dropdown-item">Mysql, PostgreSQL, SQL Server or NoSQL Set up</a></li>
+		</ul>
+</li>
+		<li id="nav-menu-item-580" class="  nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/web-development/back-end-development/php/" class="menu-link dropdown-item">PHP Custom Applications</a></li>
+		<li id="nav-menu-item-581" class=" nav-item dropdown nav-item-even nav-item-depth-2 menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children hvr-buzz-out has-dropdown not-click"><a href="//localhost:3000/services/web-development/front-end-development/" class="menu-link nav-link dropdown-toggle">Front-end Development</a>
+		<ul class="dropdown-menu dropdown-odd dropdown-child dropdown-depth-3">
+			<li id="nav-menu-item-582" class="  nav-item-odd nav-item-depth-3 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/web-development/front-end-development/css3-html5-javascript/" class="menu-link dropdown-item">CSS3, HTML5, Javascript and Javascript Frameworks</a></li>
+			<li id="nav-menu-item-585" class="  nav-item-odd nav-item-depth-3 menu-item menu-item-type-post_type menu-item-object-page hvr-buzz-out"><a href="//localhost:3000/services/web-development/front-end-development/web-design/" class="menu-link dropdown-item">Web Design (UI/UX)</a></li>
+		</ul>
+</li>
+	</ul>
+</li>
+</ul>
+</li>
+<li id="nav-menu-item-115" class="navbar-main  nav-item-even nav-item-depth-0 menu-item menu-item-type-post_type menu-item-object-page"><a href="//localhost:3000/team/" class="menu-link dropdown-item">Team</a></li>
+<li id="nav-menu-item-113" class="navbar-main  nav-item-even nav-item-depth-0 menu-item menu-item-type-post_type menu-item-object-page"><a href="//localhost:3000/blog/" class="menu-link dropdown-item">Blog</a></li>
+<li id="nav-menu-item-114" class="navbar-main  nav-item-even nav-item-depth-0 menu-item menu-item-type-post_type menu-item-object-page"><a href="//localhost:3000/contact/" class="menu-link dropdown-item">Contact</a></li>
+</ul><ul class="right topbar-more-info-nav hide-for-medium-down">
+	<li><a href="tel:+6262325218">626.232.5218</a></li>
+	<li class="has-form top-form"><div class="row collapse"><div class="large-8 small-9 columns"><input type="text" placeholder="Search LMSEO" class="radius-left"></div><div class="large-4 small-3 columns"><a href="/" class="top-button radius-right">Search</a></div></div>
+	</li>
+</ul></div></nav>
+*/
 ?>
+
