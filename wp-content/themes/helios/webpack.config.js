8dd9a62e932157b5d5681c0525212213e06bd742
Luis <luis@lmseo.co>
2023-03-24 18:53:23 -0700

    Setup services pages category in webpack, prune previous internal unused scss files

--- wp-content/themes/helios/webpack.config.js
+++ wp-content/themes/helios/webpack.config.js
@@ -1,92 +1,180 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const WebpackBar = require("webpackbar");
+const path = require("path");
 
-// change these variables to fit your project
-const jsPath= './js';
-const cssPath = './scss/';
-const outputPath = 'dist';
-const localDomain = 'http://lmseo.test';
-const entryPoints = {
-    // 'app' is the output name, people commonly use 'bundle'
-    // you can have more than 1 entry point
-    'app': jsPath + '/app.js',
-    'style': cssPath + '/style.scss',
+const outputPath = "dist";
+const localDomain = "http://lmseo.test";
+const homePageEntryPoints = {
+  app: "./js/app.js",
+  style: "./scss/style.scss",
+};
+const servicesEntryPoints = {
+  app: "./js/internal/services/services.js",
+  style: "./scss/internal/services/services.scss",
 };
 
-module.exports = {
-    entry: entryPoints,
-    devtool: 'source-map',
+module.exports = [
+  {
+    entry: homePageEntryPoints,
+    devtool: "source-map",
+    resolve: {
+      extensions: ["*", ".js"],
+    },
     output: {
-        path: path.resolve(__dirname, outputPath),
-        filename: 'homepage/js/[name].js',
-        assetModuleFilename: 'homepage/images/[name][ext][query]'
+      path: path.resolve(__dirname, outputPath),
+      filename: "homepage/js/[name].js",
+      assetModuleFilename: "homepage/images/[name][ext][query]",
     },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: '../[name].css',
-        }),
-
-        // Uncomment this if you want to use CSS Live reload
-
-        new BrowserSyncPlugin({
-            proxy: localDomain,
-            files: [  '/*.css' ],
-            injectCss: false,
-        }, { reload: true, }),
-
+      new WebpackBar(),
+      new MiniCssExtractPlugin({
+        filename: "../[name].css",
+      }),
+      new BrowserSyncPlugin(
+        {
+          proxy: localDomain,
+          files: ["/*.css"],
+          injectCss: false,
+        },
+        { reload: true }
+      ),
     ],
     module: {
-        rules: [
+      rules: [
+        {
+          test: /\.s?[c]ss$/i,
+          use: [
             {
-                test: /\.s?[c]ss$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    "css-loader",
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: () => [
-                                    require('autoprefixer')
-                                ]
-                            }
-                        }
-                    },
-                    'sass-loader'
-                ]
+              loader: MiniCssExtractPlugin.loader,
             },
+            "css-loader",
             {
-                test: /\.sass$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sassOptions: { indentedSyntax: true }
-                        }
-                    }
-                ]
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: () => [require("autoprefixer")],
+                },
+              },
             },
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.sass$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
             {
-                mimetype: 'image/svg+xml',
-                scheme: 'data',
-                type: 'asset/resource',
-                generator: {
-                    filename: 'homepage/icons/[name]-[hash].svg[query]'
-                }
+              loader: "sass-loader",
+              options: {
+                sassOptions: { indentedSyntax: true },
+              },
             },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ]
+          ],
+        },
+        {
+          mimetype: "image/svg+xml",
+          scheme: "data",
+          type: "asset/resource",
+          generator: {
+            filename: "homepage/icons/[name]-[hash].svg[query]",
+          },
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
     },
-};
+  },
+  {
+    entry: servicesEntryPoints,
+    devtool: "source-map",
+    resolve: {
+      extensions: ["*", ".js"],
+    },
+    output: {
+      path: path.resolve(__dirname, outputPath),
+      filename: "internal/services/js/[name].js",
+      assetModuleFilename: "internal/services/images/[name][ext][query]",
+    },
+    plugins: [
+      new WebpackBar(),
+      new MiniCssExtractPlugin({
+        filename: outputPath + "internal/services/[name].css",
+      }),
+      new BrowserSyncPlugin(
+        {
+          proxy: localDomain,
+          files: ["/*.css"],
+          injectCss: false,
+        },
+        { reload: true }
+      ),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.s?[c]ss$/i,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: () => [require("autoprefixer")],
+                },
+              },
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sassOptions: {
+                  includePaths: ["node_modules/bootstrap/scss/"],
+                },
+              },
+            },
+          ],
+        },
+        {
+          mimetype: "image/svg+xml",
+          scheme: "data",
+          type: "asset/resource",
+          generator: {
+            filename: "internal/services/icons/[name]-[hash].svg[query]",
+          },
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+  },
+];
